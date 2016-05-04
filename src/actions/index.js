0046665d5e77a38f54826f163884b32b9b609c46
kylefdoherty <kyledoherty22@gmail.com>
2016-05-03 23:15:39 -0400

    Add axios and redux-thunk and post dog info

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,18 @@
+import axios from 'axios';
+
 export const SUBMIT_DOG_INFO = 'SUBMIT_DOG_INFO';
 export const SUBMIT_USER_INFO = 'SUBMIT_USER_INFO';
 
 export const submitDogInfo = (info) => {
-  console.log('dog info: ', info);
-  // make POST request here & use middleware to wait
-  // for success or failure to be returned
+  const url = 'http://localhost:8000/dogs'
+  const request = axios.post(url, info)
 
-  // for now fake the data being returned on successful post
-  let request = info
-
-  return {
-    type: SUBMIT_DOG_INFO,
-    payload: request
-  }
+  return (dispatch) => {
+    request.then(({data}) => {
+      console.log(data)
+      dispatch({type: SUBMIT_DOG_INFO, payload: data})
+    });
+  };
 }
 
 export const submitUserInfo = (info) => {
